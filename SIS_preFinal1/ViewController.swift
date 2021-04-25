f3199f875703e8117ae5c08702ec190c26cf8289
SashaEee <77224274+SashaEee@users.noreply.github.com>
2021-04-25 22:21:46 +0300

    Initial Commit

---
+++ SIS_preFinal1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SIS_preFinal1
+//
+//  Created by Sasha on 4/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
