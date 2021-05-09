b00b914d9ca890c5eedf2f661344ada70d838081
SashaEee <77224274+SashaEee@users.noreply.github.com>
2021-05-09 12:43:40 +0300

    1.2

--- SIS_preFinal1/AppDelegate.swift
+++ SIS_preFinal1/AppDelegate.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         Thread.sleep(forTimeInterval: 2.0)
 		// Override point for customization after application launch.
 		return true
